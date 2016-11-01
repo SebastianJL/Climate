c4c0291828a77312b82371fda9b1dc1b3e9d86d7
Johannes Lade <johannes.lade@yahoo.com>
2016-11-01 11:08:11 +0100

    Added Entry Class (empty) for Climate module.

---
+++ Climate/src/ch/uzh/ifi/climate/client/Climate.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.climate.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Climate implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
