068604c59c39d27c7f1ab7651bc6feb35d4636a3
Johannes Lade <johannes.lade@yahoo.com>
2016-11-01 12:40:10 +0100

    Added test folder with dummy files

---
+++ Climate/test/ch/uzh/ifi/climate/client/ClimateTest.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.climate.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ClimateTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testOnModuleLoad() {
+		fail("Not yet implemented");
+	}
+
+}
