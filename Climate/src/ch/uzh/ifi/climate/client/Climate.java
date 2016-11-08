a8af2b493045b140a910eb470f6a91d42b221bd9
CarmenChristen <carmi.christen@hotmail.com>
2016-11-08 10:56:38 +0100

    basic code to show dummy message

--- Climate/src/ch/uzh/ifi/climate/client/Climate.java
+++ Climate/src/ch/uzh/ifi/climate/client/Climate.java
@@ -1,13 +1,18 @@
 package ch.uzh.ifi.climate.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Climate implements EntryPoint {
+	private VerticalPanel mainPanel = new VerticalPanel();
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
+		// Associate the Main panel with the HTML host page.
+	    RootPanel.get("stockList").add(mainPanel);
 
 	}
+
 
 }
