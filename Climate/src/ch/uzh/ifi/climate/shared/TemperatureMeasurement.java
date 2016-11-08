299476dde86f78418695540986369ad435c80b52
Johannes Lade <johannes.lade@yahoo.com>
2016-11-08 09:28:26 +0100

    Added class documentation + coordinates field

--- Climate/src/ch/uzh/ifi/climate/shared/TemperatureMeasurement.java
+++ Climate/src/ch/uzh/ifi/climate/shared/TemperatureMeasurement.java
@@ -2,18 +2,31 @@ package ch.uzh.ifi.climate.shared;
 
 import java.util.Date;
 
+
+/**
+ * This class manages a temperature measurement (temperature+uncertainty) together with its
+ * metadata (date, location, etc).
+ * @author		Johannes Lade
+ * @history 	2016-08-11 JL First version
+ * @version 	2016-08-11 JL 0.1.0
+ * @responsibilities
+ * 				This class manages a temperature measurement (temperature+uncertainty)
+ * 				together with its metadata (date, location, etc).
+ */
 public class TemperatureMeasurement{
 	private Temperature temperature;
 	private Temperature uncertainty;
 	private Date date;
 	private String city;
 	private String country;
+	private Coordinates coordinates;
 	
-	public TemperatureMeasurement(Temperature temperature, Temperature uncertainty, Date date, String city, String country) {
+	public TemperatureMeasurement(Temperature temperature, Temperature uncertainty, Date date, String city, String country, Coordinates coordinates) {
 		this.temperature = temperature;
 		this.uncertainty = uncertainty;
 		this.date = date;
 		this.city = city;
 		this.country = country;
+		this.coordinates = coordinates;
 	}
 }
