e64afb07658b510f4b18ae3cced1c545106a3470
PascalSiemon <pascal.siemon@hotmail.com>
2016-11-14 11:36:15 +0100

    Added functions to interfaces

--- Climate/src/ch/uzh/ifi/climate/client/QueryService.java
+++ Climate/src/ch/uzh/ifi/climate/client/QueryService.java
@@ -13,5 +13,10 @@ import ch.uzh.ifi.climate.shared.TemperatureMeasurement;
 public interface QueryService extends RemoteService {
 	
 	public ArrayList<TemperatureMeasurement> temperatureMeasurements(String city, Date sdate, Date edate);
-	
+	public ArrayList<TemperatureMeasurement> temperatureMeasurements(String city);
+	public ArrayList<TemperatureMeasurement> temperatureMeasurementsCountry(String country, Date sdate, Date edate);
+	public ArrayList<TemperatureMeasurement> temperatureMeasurementsCountry(String country);
+	public ArrayList<TemperatureMeasurement> clearMeasurements();
+	public ArrayList<String> getCities();
+	public ArrayList<String> getCountries();
 }
