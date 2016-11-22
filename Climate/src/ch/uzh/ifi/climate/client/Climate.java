6350ea2601b148ecd2e904a150503b40cf8b141b
Johannes Lade <johannes.lade@yahoo.com>
2016-11-22 10:01:26 +0100

    Variable renamed and declared final
    Declared variable <months> as final and renamed it to <MONTHS>.

--- Climate/src/ch/uzh/ifi/climate/client/Climate.java
+++ Climate/src/ch/uzh/ifi/climate/client/Climate.java
@@ -46,7 +46,7 @@ public class Climate implements EntryPoint {
 	private Button addFilterButton = new Button("Add");
 	private QueryServiceAsync querySvc = GWT.create(QueryService.class);
 	MultiWordSuggestOracle cityNames = new MultiWordSuggestOracle();
-	private String[] months = {"January","February","March","April","May","June",
+	private final String[] MONTHS = {"January","February","March","April","May","June",
 	                           "July","August","September","October","November","December"};
 
 	@Override
@@ -78,9 +78,9 @@ public class Climate implements EntryPoint {
 		// Add months to Month selection dropDown menu
 		startMonth.setVisibleItemCount(1);
 		endMonth.setVisibleItemCount(1);
-		for(int i = 0; i<months.length; i++){
-			startMonth.addItem(months[i]);
-			endMonth.addItem(months[i]);
+		for(int i = 0; i<MONTHS.length; i++){
+			startMonth.addItem(MONTHS[i]);
+			endMonth.addItem(MONTHS[i]);
 		}
 		
 		// Assemble Add filter panel.
