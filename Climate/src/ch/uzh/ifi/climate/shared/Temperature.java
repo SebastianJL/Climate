633222a2e74f6407a660b18cca43b703f6d96ad5
Johannes Lade <johannes.lade@yahoo.com>
2016-11-27 22:33:19 +0100

    Deleted double method.

--- Climate/src/ch/uzh/ifi/climate/shared/Temperature.java
+++ Climate/src/ch/uzh/ifi/climate/shared/Temperature.java
@@ -31,10 +31,6 @@ public class Temperature implements Serializable{
 		return new Temperature(temperatureInKelvin);
 	}
 	
-	public double getTemperatureInKelvin(){
-		return this.temperatureInKelvin;
-	}
-	
 	/**
 	 * @pre -
 	 * @post -
