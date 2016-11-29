602f1d6d72e361c727182a0cd2cc60c4def27cf4
PascalSiemon <pascal.siemon@hotmail.com>
2016-11-29 07:56:25 +0100

    No important changes

---
+++ Climate/test/ch/uzh/ifi/climate/server/QueryServiceImplTest.java
@@ -0,0 +1,29 @@
+package ch.uzh.ifi.climate.server;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import ch.uzh.ifi.climate.shared.Coordinates;
+import ch.uzh.ifi.climate.shared.Temperature;
+import ch.uzh.ifi.climate.shared.TemperatureMeasurement;
+import ch.uzh.ifi.climate.server.QueryServiceImpl;
+
+public class QueryServiceImplTest{
+	
+	@Test
+	public void testFilterCity(){
+		
+	}
+	
+	@Test
+	public void testFilterCityDates(){
+		
+	}
+}
