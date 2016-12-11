5a17ee3f241e62abbbb180c642a543e802ce3dcf
PascalSiemon <pascal.siemon@hotmail.com>
2016-12-11 21:33:29 +0100

    Tried to set up JUnit tests for QueryServiceImpl

--- Climate/test/ch/uzh/ifi/climate/server/QueryServiceImplTest.java
+++ Climate/test/ch/uzh/ifi/climate/server/QueryServiceImplTest.java
@@ -16,14 +16,24 @@ import ch.uzh.ifi.climate.shared.TemperatureMeasurement;
 import ch.uzh.ifi.climate.server.QueryServiceImpl;
 
 public class QueryServiceImplTest{
+	//private QueryServiceImpl testQueryService = new QueryServiceImpl();
 	
+	private final TemperatureMeasurement TEST_MEASUREMENT_1 = 
+			new TemperatureMeasurement(	Temperature.createFromCelsius(10.3f),
+										Temperature.createFromCelsius(0.2f),
+										new Date(),
+										"TestCity1",
+										"TestCountry1",
+										new Coordinates(10.2f, -40.32f));
+	private final TemperatureMeasurement TEST_MEASUREMENT_2 = 
+			new TemperatureMeasurement(	Temperature.createFromCelsius(20.2f),
+										Temperature.createFromCelsius(1.3f),
+										new Date(),
+										"TestCity2",
+										"TestCountry2",
+										new Coordinates(-32.6f, 52.9f));
 	@Test
 	public void testFilterCity(){
-		
-	}
-	
-	@Test
-	public void testFilterCityDates(){
 		
 	}
 }
